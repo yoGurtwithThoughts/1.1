1aea927842603aff9c471aadb67e434c529b35c1
SHPAK <gaylord@gmail.com>
2023-09-29 10:56:54 +0300

    Добавьте файлы проекта.

---
+++ 1.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int a, b;
+    cout << "\tВведите a\t" << endl;
+    cin >> a;
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        cout << "Вы ввели не число!" << endl;
+    }
+    else {
+        cout << "Вы ввели число " << a << endl;
+    }
+    //if (typeid(a).name() != "int")
+    //{
+    //    cout << "Тип данных не int" << endl;
+    //}
+    //else
+    //{
+    //    cout << "OK" << endl;
+    //}
+
